a0cc5c98e193d1dabc1533a1040f4d409ddb3cb1
pranavtaysheti <pranavtaysheti@gmail.com>
2023-10-20 17:14:46 +0530

    update flutter embedding and build system

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,75 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
-        ),
-        body: DicePage(),
-      ),
-    ),
+    const MyApp(),
   );
 }
 
-class DicePage extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({
+    super.key,
+  });
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: const Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: const DicePage(),
+      ),
+    );
+  }
+}
+
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int randomDice() => Random().nextInt(6) + 1;
+
+  int leftDiceNumber = 3;
+  int rightDiceNumber = 3;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  leftDiceNumber = randomDice();
+                  rightDiceNumber = randomDice();
+                });
+              },
+              child: Image.asset("images/dice$leftDiceNumber.png"),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  rightDiceNumber = randomDice();
+                  leftDiceNumber = randomDice();
+                });
+              },
+              child: Image.asset("images/dice$rightDiceNumber.png"),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
